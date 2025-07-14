ad90d60c75c25951a60893c43b9e3116665b5f79
DarckList <sashagigabyte@gmail.com>
2025-07-14 10:25:11 +0300

    K: credits

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,11 @@
-# TODO: write the script
+echo "\n==== SCANNING FOR CREDIT CARD NUMBERS ====="
+grep -rE --color=always --exclude-dir={.git} --line-number '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . 
+echo "\n==== CREDIT CARD SAN COMPLETE ====="
+echo "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ====="
+grep -rE --color=always --exclude-dir={.git} --line-number '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . 
+echo "\n==== SOCIAL SECURITY SCAN COMPLETE ====="
+
+echo '\n==== SCANNING FOR PHONE NUMBERS ===='
+grep -rE --color=always --exclude-dir={.git} --line-number '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' .
+grep -rE --color=always --exclude-dir={.git} --line-number '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' .
+echo "==== PHONE NUMBER SCAN COMPLEE ===="
